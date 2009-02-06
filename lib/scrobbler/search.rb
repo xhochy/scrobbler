910c739733262784efd63dcc00907dbff604ca61
Kurt Schrader <kurt@karmalab.org>
2009-02-06 11:55:35 -0800

    Make sure we can use the API key for the 2.0 method calls.

--- lib/scrobbler/search.rb
+++ lib/scrobbler/search.rb
@@ -5,10 +5,15 @@
 
 module Scrobbler
   class Search < Base
-    attr_accessor :type, :query
+    attr_accessor :type, :query, :api_key
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
 
     def execute
       doc = self.class.fetch_and_parse(api_path)
+      puts doc
       albums = []
       (doc/"//album/url").each do |url|
         pieces = url.inner_html.split("/")
@@ -19,15 +24,14 @@ module Scrobbler
       albums
     end
 
-    def self.by_album(album_name)
-      search = self.new
-      search.type = 'album'
-      search.query = album_name
-      return search
+    def by_album(album_name)
+      @type = 'album'
+      @query = album_name
+      execute
     end
 
     def api_path
-      "/2.0/?method=#{type}.search&album=#{CGI::escape(query)}&api_key="
+      "/2.0/?method=#{type}.search&album=#{CGI::escape(query)}&api_key=#{api_key}"
     end
 
   end
--- test/unit/search_test.rb
+++ test/unit/search_test.rb
@@ -4,18 +4,22 @@ class TestSearch < Test::Unit::TestCase
 
   def setup
     @album_name = "Amnesiac"
-    @search = Scrobbler::Search.by_album(@album_name)
+    @api_key = "122345678"
+    @search = Scrobbler::Search.new(@api_key)
+    #@search = Scrobbler::Search.by_album(@album_name)
   end
 
   test 'should be able to search for an album and return a list of possible matches' do
-    assert_equal(20, @search.execute.size)
+    assert_equal(20, @search.by_album(@album_name).size)
   end
 
   test "should be able to correctly find the album that we're searching for" do
-    assert_equal("Make Believe", @search.execute[0].name)
+    assert_equal("Make Believe", @search.by_album(@album_name)[0].name)
   end
 
   test "should have correct api path when searching for an album" do
+    @search.type = 'album'
+    @search.query = @album_name
     assert_equal("/2.0/?method=album.search&album=#{@album_name}&api_key=#{@api_key}", @search.api_path)
   end
 
