ac8fe4a50e2202c2627f117436c7cdaebe8aae2d
Uwe L. Korn <uwelk@xhochy.org>
2009-04-17 13:02:39 +0200

    Updated test data for Scrobbler::Artist

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -4,12 +4,41 @@ class TestArtist < Test::Unit::TestCase
 
   def setup
     @artist = Scrobbler::Artist.new('Metallica')
+    
+    @similar_artists = ["Megadeth", "Pantera", "Slayer", "Iron Maiden", 
+        "Anthrax", "Machine Head", "Sepultura", "Mot\303\266rhead", "Testament",
+        "Trivium", "Black Sabbath", "Exodus", "Ozzy Osbourne", "AC/DC", 
+        "Kreator", "Black Label Society", "Guns N' Roses", "System of a Down",
+        "Judas Priest", "Soulfly", "Godsmack", "Children of Bodom", "Slipknot",
+        "Disturbed", "Hunter", "Apocalyptica", "Iced Earth", "Annihilator", 
+        "Rammstein", "Ko\320\257n", "Death Angel", "In Flames", "Lamb of God",
+        "Overkill", "Diamond Head", "Damageplan", "Acid Drinkers", "Beatallica",
+        "Stone Sour", "Metal Church", "MD.45", "Volbeat", "HammerFall", 
+        "Black Tide", "Rage Against the Machine", "Manowar", "Probot",
+        "Bullet For My Valentine", "Flotsam and Jetsam", "Dio", 
+        "Roadrunner United", "Venom", "Arch Enemy", "Mercyful Fate", 
+        "Nuclear Assault", "Destruction", "Blind Guardian", "Sacred Reich",
+        "Bruce Dickinson", "Turbo", "Static-X", "Helloween", "Zakk Wylde",
+        "Avenged Sevenfold", "W.A.S.P.", "Kat", "Skid Row", "DevilDriver",
+        "Danzig", "Heathen", "Mokoma", "Lordi", "Shadows Fall", "Chimaira",
+        "Whiplash", "Mudvayne","Onslaught", "Sodom", "Rage", "Forbidden", 
+        "Evile", "Savatage", "Scorpions", "DragonForce", "Axel Rudi Pell",
+        "Rob Zombie", "Killswitch Engage", "Sebastian Bach", "Saxon", 
+        "Cavalera Conspiracy", "Voivod", "Drowning Pool", "Fear Factory",
+        "M\303\266tley Cr\303\274e", "Prong", "Kotiteollisuus", "Stone",
+        "Fozzy", "Yngwie Malmsteen", "Deep Purple"]
+    @top_albums = ["Master of Puppets", "Metallica", "Ride the Lightning", "Reload"]
+    @top_fans = ["Slide15", "Air_Force", "Evile9", "thy_satan", "Aiham", "Quugel"]
+    @top_tracks = ['Nothing Else Matters', 'Enter Sandman', 
+        'The Day That Never Comes', 'One']
   end
   
+  # @apiversion 2.0
   test 'should require name' do
     assert_raises(ArgumentError) { Scrobbler::Artist.new('') }
   end
   
+  # @apiversion 2.0
   test "should know it's name" do
     assert_equal('Metallica', @artist.name)
   end
@@ -30,48 +59,52 @@ class TestArtist < Test::Unit::TestCase
     assert_equal('http://ws.audioscrobbler.com/1.0/artist/Metallica/events.rss', @artist.current_events(:rss))
   end
   
+  # @apiversion 2.0
   test 'should be able to find similar artists' do
-    assert_equal(['Iron Maiden', 'System of a Down', "Guns N' Roses", 'Rammstein'], @artist.similar.collect(&:name))
+    assert_equal(@similar_artists, @artist.similar.collect(&:name))
     first = @artist.similar.first
-    assert_equal('Iron Maiden', first.name)
-    assert_equal('ca891d65-d9b0-4258-89f7-e6ba29d83767', first.mbid)
+    assert_equal('Megadeth', first.name)
+    assert_equal('a9044915-8be3-4c7e-b11f-9e2d2ea0a91e', first.mbid)
     assert_equal('100', first.match)
-    assert_equal('http://www.last.fm/music/Iron+Maiden', first.url)
-    assert_equal('http://static3.last.fm/storable/image/177412/small.jpg', first.thumbnail)
-    assert_equal('http://panther1.last.fm/proposedimages/sidebar/6/1000107/528560.jpg', first.image)
+    assert_equal('www.last.fm/music/Megadeth', first.url)
+    assert_equal('http://userserve-ak.last.fm/serve/34/8422011.jpg', first.thumbnail)
+    assert_equal('http://userserve-ak.last.fm/serve/64/8422011.jpg', first.image)
+    assert_equal('http://userserve-ak.last.fm/serve/126/8422011.jpg', first.image_large)
     assert_equal('yes', first.streamable)
   end
   
+  # @apiversion 2.0
   test 'should be able to find top fans' do
-    assert_equal(%w{Kikoin Chrisis92 delekt0r}, @artist.top_fans.collect(&:username))
+    assert_equal(@top_fans, @artist.top_fans.collect(&:username))
     first = @artist.top_fans.first
-    assert_equal('Kikoin', first.username)
-    assert_equal('http://www.last.fm/user/Kikoin/', first.url)
-    assert_equal('http://panther1.last.fm/avatar/621340085aed19b9ab77a0f8c575758a.gif', first.avatar)
-    assert_equal('6081000', first.weight)
+    assert_equal('Slide15', first.username)
+    assert_equal('http://www.last.fm/user/Slide15', first.url)
+    assert_equal('http://userserve-ak.last.fm/serve/34/4477633.jpg', first.avatar)
+    assert_equal('265440672', first.weight)
   end
   
+  # @apiversion 2.0
   test 'should be able to find top tracks' do
-    assert_equal(['Nothing Else Matters', 'Enter Sandman', 'Master of Puppets', 'One'], @artist.top_tracks.collect(&:name))
+    assert_equal(@top_tracks, @artist.top_tracks.collect(&:name))
     first = @artist.top_tracks.first
     assert_equal('Nothing Else Matters', first.name)
     assert_equal('', first.mbid)
     assert_equal('http://www.last.fm/music/Metallica/_/Nothing+Else+Matters', first.url)
-    assert_equal('59135', first.reach)
   end
   
+  # @apiversion 2.0
   test 'should be able to find top albums' do
-    assert_equal(['Master of Puppets', 'Metallica', 'Reload'], @artist.top_albums.collect(&:name))
+    assert_equal(@top_albums, @artist.top_albums.collect(&:name))
     first = @artist.top_albums.first
     assert_equal('Master of Puppets', first.name)
     assert_equal('fed37cfc-2a6d-4569-9ac0-501a7c7598eb', first.mbid)
-    assert_equal('65717', first.reach)
     assert_equal('http://www.last.fm/music/Metallica/Master+of+Puppets', first.url)
-    assert_equal('http://cdn.last.fm/coverart/50x50/1411810.jpg', first.image(:small))
-    assert_equal('http://cdn.last.fm/coverart/130x130/1411810.jpg', first.image(:medium))
-    assert_equal('http://panther1.last.fm/coverart/130x130/1411810.jpg', first.image(:large))    
+    assert_equal('http://userserve-ak.last.fm/serve/34s/8622967.jpg', first.image(:small))
+    assert_equal('http://userserve-ak.last.fm/serve/64s/8622967.jpg', first.image(:medium))
+    assert_equal('http://userserve-ak.last.fm/serve/126/8622967.jpg', first.image(:large))    
   end
   
+  # @apiversion 2.0
   test 'should be able to find top tags' do
     assert_equal(['metal', 'thrash metal', 'heavy metal'], @artist.top_tags.collect(&:name))
     first = @artist.top_tags.first
