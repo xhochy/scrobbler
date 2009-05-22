44282e5183c76e38ae61aa922e2f5981e3550f15
James Cotterill <jamescotterill@hotmail.com>
2009-05-23 00:26:57 +0100

    finished the getshouts api call

--- lib/scrobbler/shout.rb
+++ lib/scrobbler/shout.rb
@@ -6,7 +6,18 @@ module Scrobbler
   class Shout < Base
     class << self
       def new_from_libxml(xml)
+        data={}
+        xml.children.each do |child|
+          data[:body] = child.content if child.name == 'body'
+          data[:author] = child.content if child.name == 'author'
+          data[:date] = child.content if child.name == 'date'
+        end
+        Shout.new(data)
       end
+    end
+
+    def initialize(input={})
+      populate_data(input)
     end
   end
 end
