58e0035ddf66774e709c24b300777f038f68cbee
James Cotterill <jamescotterill@hotmail.com>
2009-05-31 12:33:31 +0100

    fixed some of the geo tests

--- test/unit/geo_test.rb
+++ test/unit/geo_test.rb
@@ -6,7 +6,7 @@ class TestGeo < Test::Unit::TestCase
     @geo = Scrobbler::Geo.new
     @events = ['Will and The People','Son Of Dave','Surface Unsigned',
       'Experimental Dental School']
-    @ids = ['1025661','954053','1005964','909456']
+    @ids = [1025661,954053,1005964,909456]
     @first_atrists_names = ['Will And The People','Carnations','Midwich Cuckoos','NO FLASH']
     @first_headliner = 'Will And The People'
     @top_artist_names = ['The Killers','Coldplay','Radiohead','Muse','Franz Ferdinand','U2']
@@ -29,10 +29,10 @@ class TestGeo < Test::Unit::TestCase
     assert_equal('http://userserve-ak.last.fm/serve/64/24035067.jpg', first.image_medium)
     assert_equal('http://userserve-ak.last.fm/serve/126/24035067.jpg', first.image_large)    
     assert_equal('Sed sed lectus hendrerit urna imperdiet bibendum.', first.description)
-    assert_equal('2', first.attendance)
-    assert_equal('0', first.reviews)
+    assert_equal(2, first.attendance)
+    assert_equal(0, first.reviews)
     assert_equal('lastfm:event=1025661', first.tag)
-    assert_equal('Sun, 10 May 2009', first.start_date)
+    assert_equal(Time.parse('Sun, 10 May 2009'), first.start_date)
     assert_equal('19:30', first.start_time)
   end
 
@@ -56,7 +56,7 @@ class TestGeo < Test::Unit::TestCase
   test 'first top artist has the correct attributes' do
     top_artist = @geo.top_artists('spain').first
     assert_equal('The Killers',top_artist.name)
-    assert_equal('2635',top_artist.playcount)
+    assert_equal(2635,top_artist.playcount)
     assert_equal('95e1ead9-4d31-4808-a7ac-32c3614c116b',top_artist.mbid)
     assert_equal('http://www.last.fm/music/The+Killers',top_artist.url)
     assert_equal(true,top_artist.streamable)
@@ -73,7 +73,7 @@ class TestGeo < Test::Unit::TestCase
   test 'first top track has the correct attributes' do
     top_track = @geo.top_tracks('germany').first
     assert_equal('Use Somebody',top_track.name)
-    assert_equal('2819',top_track.playcount)
+    assert_equal(2819,top_track.playcount)
     assert_equal('1234567abcde',top_track.mbid)
     assert_equal('http://www.last.fm/music/Kings+of+Leon/_/Use+Somebody',top_track.url)    
     assert_equal('Kings of Leon',top_track.artist.name)
