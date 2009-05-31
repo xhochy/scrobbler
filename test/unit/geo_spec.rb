10a1ab7d5082afb218213e1348842d2a2259a7f8
James Cotterill <jamescotterill@hotmail.com>
2009-05-31 12:59:06 +0100

    added to the goe spec

--- test/unit/geo_spec.rb
+++ test/unit/geo_spec.rb
@@ -1,7 +1,60 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe Scrobbler::Geo do
+  
+  before do
+    @geo = Scrobbler::Geo.new
+    @event_titles = ['Will and The People','Son Of Dave','Surface Unsigned','Experimental Dental School']
+    @event_ids = [1025661,954053,1005964,909456]
+    @first_atrists_names = ['Will And The People','Carnations','Midwich Cuckoos','NO FLASH']
+    @first_headliner = 'Will And The People'
+    @top_artist_names = ['The Killers','Coldplay','Radiohead','Muse','Franz Ferdinand','U2']
+    @top_track_names = ['Use Somebody','Schwarz zu Blau','Sex on Fire','Alles Neu','Poker Face','Ayo Technology']
+  end
  
- it 'needs a spec written for it'
+  describe 'finding events in manchester' do
+    before do
+      @events =  @geo.events('Manchester')
+    end
 
+    it 'should find 4 events' do
+      @events.size.should == 4
+    end
+   
+    it "should have the correct event id's" do
+      @events.collect(&:id).should == @event_ids
+    end
+
+    it 'should have the correct event titles' do
+      @events.collect(&:title).should == @event_titles
+    end
+  end
+
+  describe 'finding top artists in spain' do
+    before do
+      @top_artists = @geo.top_artists('spain')
+    end
+
+    it 'should find 6 artists' do
+      @top_artists.size.should == 6
+    end
+
+    it 'should have the correct artist names' do      
+      @top_artists.collect(&:name).should == @top_artist_names
+    end
+  end
+
+  describe 'finding top tracks' do
+    before do
+      @top_tracks = @geo.top_tracks('germany')
+    end
+
+    it 'should find 6 top tracks' do
+      @top_tracks.size.should == 6
+    end
+
+    it 'should have the correct top track names' do
+      @top_track_names.should == @top_tracks.collect(&:name)
+    end
+  end
 end
