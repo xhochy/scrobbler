2f64fb922790f532c17709940281d3b5c87d1312
Uwe L. Korn <uwelk@xhochy.com>
2010-07-13 16:30:01 +0200

    Added scaffolds for authentication unit tests

---
+++ test/unit/authentication_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+describe Scrobbler::Authentication do
+
+  it 'should be able to authorize a mobile session'
+
+  it 'should be able to authorize a web session'
+
+  it 'should be able to authroize a desktop session'
+
+end
+
